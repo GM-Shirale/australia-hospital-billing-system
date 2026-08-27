ab755bebfd9cc7f797498a0e5c11cf8c2dd5c4be
Ganga18062001 <gangashirale@gmail.com>
2026-08-27 14:19:00 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/hospital/hospital_billing_system/HospitalBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_billing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalBillingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/hospital_billing_system/HospitalBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_billing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
