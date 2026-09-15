daedd4b1bc1e1125c4890f8331bb45c3b76b60d2
SumitAdbane02 <sumitadbane02@gmail.com>
2026-09-15 14:36:24 +0530

    Remove patient module from laboratory branch

--- src/main/java/com/hospital/hospital_billing_system/patient/repository/PatientAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.hospital_billing_system.patient.repository;
-
-import com.hospital.hospital_billing_system.patient.entity.PatientAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientAddressRepository extends JpaRepository<PatientAddress, Long> {
-
-}
